e14c0a50c6bb9290ca4be2d590a7b7686c8bf94c
andreorvalho <andre.orvalho@getgears.com>
2014-01-23 16:32:42 +0100

    added tests for before validations

--- lib/image_system/concerns/image.rb
+++ lib/image_system/concerns/image.rb
@@ -8,18 +8,41 @@ module ImageSystem
       attr_accessor :source_file_path
 
       included do
+        # validations
         validates :uuid, presence: true
         validates :source_file_path, presence: true, on: :create
         validates :width, presence: true
         validates :height, presence: true
 
+        # callbacks
         before_validation :set_uuid, on: :create
+        before_validation :upload_to_system
       end
 
     private
 
+      def rescue_from_cdn_failure(&block)
+        begin
+          block.call
+        rescue Exceptions::CdnResponseException => e
+          # should log the problem
+          self.errors.add(:image, "The photo could not be uploaded")
+        rescue Exceptions::CdnUnknownException => e
+          # should log the problem
+          self.errors.add(:image, "The photo could not be uploaded")
+        end
+      end
+
       def set_uuid
         self.uuid = UUIDTools::UUID.random_create.to_s.gsub(/\-/, '')
+      end
+
+      def upload_to_system
+        rescue_from_cdn_failure do
+          if self.new_record? || self.changed.include?("uuid")
+            CDN::CommunicationSystem.upload(uuid: self.uuid, source_file_path: self.source_file_path, queue_processing: false)
+          end
+        end
       end
 
     end
--- spec/lib/image_system/concerns/image_spec.rb
+++ spec/lib/image_system/concerns/image_spec.rb
@@ -2,58 +2,103 @@ require 'spec_helper'
 
 describe ImageSystem::Concerns::Image do
 
-  let(:photo) { Photo.new(uuid: create_uuid, source_file_path: test_image_path, height: 100, width: 100) }
+  let(:new_photo) { build(:photo, uuid: create_uuid, source_file_path: test_image_path, height: 100, width: 100 ) }
+  let(:photo) { create(:photo, uuid: create_uuid, source_file_path: test_image_path, height: 100, width: 100) }
 
   describe "#validations" do
 
+    before(:each) do
+      ImageSystem::CDN::CommunicationSystem.stub(:upload)
+    end
+
     it "does not validate an image without the presence of uuid" do
-      photo.save
       photo.uuid = nil
       expect(photo).to_not be_valid
     end
 
     it "does not validate an image without the presence of source_file_path" do
-      photo.source_file_path = nil
-      expect(photo).to_not be_valid
+      new_photo.source_file_path = nil
+      expect(new_photo).to_not be_valid
     end
 
     it "Validate an image without the presence of source_file_path if its not a new record" do
-      expect(photo).to be_valid
-      photo.save
       photo.source_file_path = nil
       photo.save
       expect(photo).to be_valid
     end
 
     it "does not validate an image without the presence of width" do
-      photo.width = nil
-      expect(photo).to_not be_valid
+      new_photo.width = nil
+      expect(new_photo).to_not be_valid
     end
 
     it "does not validate an image without the presence of height" do
-      photo.height = nil
-      expect(photo).to_not be_valid
+      new_photo.height = nil
+      expect(new_photo).to_not be_valid
     end
 
     it "validates an image if uuid and source_file_path is present" do
-      expect(photo).to be_valid
+      expect(new_photo).to be_valid
     end
   end
 
   describe "before_validations" do
-    it "sets uuid if is not set" do
-      new_photo = Photo.create(source_file_path: test_image_path, height: 100, width: 100)
-      expect(new_photo.uuid).to_not be_nil
-      expect(new_photo).to be_valid
+
+    describe "set_uuid" do
+
+      before(:each) do
+        ImageSystem::CDN::CommunicationSystem.stub(:upload)
+      end
+
+      it "sets uuid if is not set" do
+        expect(new_photo.uuid).to_not be_nil
+        expect(new_photo).to be_valid
+      end
+
+      it "if uuid has been set, should set another one." do
+        uuid = new_photo.uuid
+        new_photo.save
+        expect(new_photo.uuid).to_not eq(uuid)
+        expect(new_photo).to be_valid
+      end
     end
 
-    it "if uuid has been set, should set another one." do
-      uuid = photo.uuid
-      photo.save
-      expect(photo.uuid).to_not eq(uuid)
-      expect(photo).to be_valid
+    describe "upload_to_system" do
+      it "does not save an image that is new and has not received a response from cdn" do
+        ImageSystem::CDN::CommunicationSystem.stub(:upload).and_raise(Exceptions::CdnResponseException.new("http_response was nil"))
+        ImageSystem::CDN::CommunicationSystem.should_receive(:upload)
+        expect(new_photo.save).to eq(false)
+        expect(new_photo.errors.full_messages).to eq(["Image The photo could not be uploaded"])
+      end
+
+      it "does not save an image that is new and has not been uploaded successfully for unknown reasons" do
+        ImageSystem::CDN::CommunicationSystem.stub(:upload).and_raise(Exceptions::CdnUnknownException.new("cdn communication system failed"))
+        ImageSystem::CDN::CommunicationSystem.should_receive(:upload)
+        expect(new_photo.save).to eq(false)
+        expect(new_photo.errors.full_messages).to eq(["Image The photo could not be uploaded"])
+      end
+
+      it "saves an image that is new and has been uploaded successfully" do
+        ImageSystem::CDN::CommunicationSystem.should_receive(:upload)
+        expect(new_photo.save).to eq(true)
+      end
+
+      it "does not upload a new image that is not a new record and does not have a new uuid" do
+        ImageSystem::CDN::CommunicationSystem.stub(:upload)
+        expect(photo).to_not be_a_new_record
+
+        ImageSystem::CDN::CommunicationSystem.should_not_receive(:upload)
+        expect(photo.save).to eq(true)
+      end
+
+      it "uploads the image if its not a new record but has a new uuid" do
+        ImageSystem::CDN::CommunicationSystem.stub(:upload)
+        expect(photo).to_not be_a_new_record
+
+        photo.uuid = create_uuid
+        ImageSystem::CDN::CommunicationSystem.should_receive(:upload)
+        photo.save
+      end
     end
   end
-
-
 end
