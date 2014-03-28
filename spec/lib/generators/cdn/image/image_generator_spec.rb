fc69ccd293eb308ae1e1e520668182003a793eeb
andreorvalho <andre.orvalho@getgears.com>
2014-03-28 10:56:14 +0100

    updated gems and ruby version and tests are working now

--- spec/lib/generators/cdn/image/image_generator_spec.rb
+++ spec/lib/generators/cdn/image/image_generator_spec.rb
@@ -3,7 +3,7 @@ require "generators/cdn/image/image_generator"
 require "generator_spec"
 
 describe Cdn::Generators::ImageGenerator do
-  destination File.expand_path("../../tmp", __FILE__)
+  destination File.join(Rails.root, "/tmp")
 
   before(:all) do
     prepare_destination
@@ -11,8 +11,7 @@ describe Cdn::Generators::ImageGenerator do
 
   context "When the model exists" do
     before(:each) do
-      @args = File.join(File.expand_path("../../tmp", __FILE__), File.join("app", "models", "picture.rb"))
-      allow(File).to receive(:exists?).with(@args).and_return(true) { File.unstub(:exists?) }
+      stub_model_file_and_unstub_after("picture", true)
       run_generator %w(picture)
     end
 
@@ -35,12 +34,12 @@ describe Cdn::Generators::ImageGenerator do
 
     it "runs generator and correct files are created" do
       assert_migration "db/migrate/create_pictures_with_cdn_fields.rb", /def change/
-      assert_migration "db/migrate/add_cdn_fields_to_pictures.rb", /change_table\(:pictures\)/
-      assert_migration "db/migrate/add_cdn_fields_to_pictures.rb", /t.string  :uuid/
-      assert_migration "db/migrate/add_cdn_fields_to_pictures.rb", /t.integer :width/
-      assert_migration "db/migrate/add_cdn_fields_to_pictures.rb", /t.integer :height/
-      assert_migration "db/migrate/add_cdn_fields_to_pictures.rb", /t.string :file_extension/
-      assert_migration "db/migrate/add_cdn_fields_to_pictures.rb", /add_index :pictures, :uuid, unique: true/
+      assert_migration "db/migrate/create_pictures_with_cdn_fields.rb", /create_table\(:pictures\)/
+      assert_migration "db/migrate/create_pictures_with_cdn_fields.rb", /t.string  :uuid/
+      assert_migration "db/migrate/create_pictures_with_cdn_fields.rb", /t.integer :width/
+      assert_migration "db/migrate/create_pictures_with_cdn_fields.rb", /t.integer :height/
+      assert_migration "db/migrate/create_pictures_with_cdn_fields.rb", /t.string :file_extension/
+      assert_migration "db/migrate/create_pictures_with_cdn_fields.rb", /add_index :pictures, :uuid, unique: true/
     end
   end
 end
