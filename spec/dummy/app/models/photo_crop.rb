68480e89f2cbb41731d82a1c6e67bcc67596ce0c
Johan Frølich <johanfrolich@gmail.com>
2014-09-17 20:26:08 +0200

    Added default values for crops

--- spec/dummy/app/models/photo_crop.rb
+++ spec/dummy/app/models/photo_crop.rb
@@ -25,7 +25,7 @@ class PhotoCrop < ActiveRecord::Base
 
   # Class Methods
   def self.aspect_ratios
-    ASPECT_RATIOS
+    ASPECT_RATIOS.with_indifferent_access
   end
 
   def self.available_aspects
@@ -34,5 +34,43 @@ class PhotoCrop < ActiveRecord::Base
 
   def self.crop_for(aspect)
     find_by_aspect(aspects[aspect])
+  end
+
+  def self.default_crop(image, aspect)
+    if aspect.to_sym == :original
+      nil
+    else
+      new_dimensions = calculate_dimensions(image.width, image.height, aspect_ratios[aspect])
+      crop = calculate_crop(image.width, image.height, new_dimensions[:width], new_dimensions[:height])
+
+      new crop.merge(aspect: aspect)
+    end
+  end
+
+private
+
+  def self.calculate_dimensions(width, height, ratio)
+    # raise "#{height} - #{width} - #{ratio}"
+    if height * ratio > width
+       max_width = width
+       max_height = width / ratio
+    else
+      max_width = height * ratio
+      max_height = height
+    end
+
+    { width: max_width, height: max_height }
+  end
+
+  def self.calculate_crop(orig_width, orig_height, new_width, new_height)
+    width_offset = (orig_width - new_width) / 2
+    height_offset = (orig_height - new_height) / 2
+
+    {
+      x1: width_offset,
+      x2: orig_width - width_offset,
+      y1: height_offset,
+      y2: orig_height - height_offset
+    }
   end
 end
